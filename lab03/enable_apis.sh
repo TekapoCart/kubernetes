d54b547edac4b1d5445e805faca0858d81579998
TekapoCart <hi@tekapo.io>
2019-08-01 10:06:22 +0800

    Lab03

--- lab03/enable_apis.sh
+++ lab03/enable_apis.sh
@@ -4,8 +4,8 @@
 
 function enable_api() {
   SERVICE=$1
-  if [[ $(gcloud services list --format="value(serviceConfig.name)" \
-                                --filter="serviceConfig.name:$SERVICE" 2>&1) != \
+  if [[ $(gcloud services list --format="value(config.name)" \
+                                --filter="$SERVICE" 2>&1) != \
                                 "$SERVICE" ]]; then
     echo "Enabling $SERVICE"
     gcloud services enable "$SERVICE"
