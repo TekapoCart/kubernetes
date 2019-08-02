ca3560c2715cc41142ca9079e45d345dabc2cb38
TekapoCart <hi@tekapo.io>
2019-08-02 12:24:25 +0800

    Lab004

--- lab004/nodejs.py
+++ lab004/nodejs.py
@@ -36,7 +36,7 @@ def GenerateConfig(context):
           # If left out will default to 1
           'size': 2,
           # If left out will default to 1
-          'maxSize': 20
+          'maxSize': {{MAX_SIZE}}
       }
   }, {
       'name': firewall,
